de77040b1df42bef06efcd8b0e54f5f4a4cae8d8
katznoah <noahkatz@gmail.com>
2021-04-02 18:45:05 -0400

    feat: made loopThrough method flexible

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-function loopThrough(string, index, direction) {
-    if(index==string.length+1) {
+function loopThrough(tag, strings, index1, index2, direction) {
+    var string = strings[index2%strings.length];
+    if(index1 == string.length+1){
         direction = 1;
-        return setTimeout(function() {loopThrough(string, index-1, direction)}, 3000);
+        return setTimeout(function() {loopThrough(tag, strings, index1-1, index2, direction)}, 1000);
     }
-    else if(index==-1) {
-        if(string=="Hello") string = "I'm Noah";
-        else string = "Hello";
-        index = 0;
+    else if(index1==-1) {
+        index2++;
+        index1 = 0;
         direction = 0;
     }
-    else document.getElementById("head").innerHTML = `${string.substring(0,index)}_`;
-    if(direction == 0) setTimeout(function() {loopThrough(string, index+1, direction)}, 150);
-    if(direction == 1) setTimeout(function() {loopThrough(string, index-1, direction)}, 150);
+    else document.getElementById("head").innerHTML = `${string.substring(0,index1)}_`;
+    if(direction == 0) setTimeout(function() {loopThrough(tag, strings, index1+1, index2, direction)}, 100);
+    else setTimeout(function() {loopThrough(tag, strings, index1-1, index2, direction)}, 50);
 }
